b5d711b6b7ee6ac525c7a7b741aa8332a44d404e
pandord24 <gordonkwan@msn.com>
2024-04-09 10:45:53 -0700

    third push

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,60 @@
 const headers = new Headers({
   "Content-Type": "application/json",
-  "x-api-key": "live_Lh6idGIX2s0O1fSTNNNZgzh7TIrEKbLJbyj3sglCt6SuVDT7mcN4JhVL2kvJA5MO"
+  "x-api-key":
+    "live_Lh6idGIX2s0O1fSTNNNZgzh7TIrEKbLJbyj3sglCt6SuVDT7mcN4JhVL2kvJA5MO",
 });
 
 var requestOptions = {
-  method: 'GET',
+  method: "GET",
   headers: headers,
-  redirect: 'follow'
+  redirect: "follow",
 };
 
 async function getBreeds() {
   let breedArr = [];
   try {
-    for (let i = 0; i <10; i++) {
-      const response = await fetch(`https://api.thedogapi.com/v1/breeds?limit=20&page=${i}`, requestOptions);
+    for (let i = 0; i < 10; i++) {
+      const response = await fetch(
+        `https://api.thedogapi.com/v1/breeds?limit=20&page=${i}`,
+        requestOptions
+      );
       const result = await response.json();
       breedArr = breedArr.concat(result);
     }
     return breedArr;
   } catch (error) {
-    console.log('Error:', error);
-    return []; // Return an empty array in case of error
+    console.log("Error:", error);
+    return [];
   }
 }
 
 async function fetchData() {
   try {
     const breedsData = await getBreeds();
-    console.log(breedsData); // This will log the actual data array
-    // Do whatever you want with the data array here
+    filterDataByUserChoice(breedsData);
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
   }
 }
+
+function filterDataByUserChoice(breedsData) {
+  console.log(breedsData);
+  const result = [];
+  breedsData.forEach((breed) => {
+    const { temperament } = breed;
+    if (temperament) {
+      const arr = temperament.split(", ");
+      console.log(arr);
+      if (arr.includes("Gay")) {
+        result.push(breed);
+      }
+      
+    }
+  });
+  console.log(result);
+}
+
+fetchData();
 
 // Get the modal
 var modal = document.getElementById("myModal");
